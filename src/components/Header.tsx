adce321357a6cabc0701cf485507253c67937e7e
Damien Vesper <ldamienvesper@gmail.com>
2021-06-18 17:47:41 -0500

    api url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-declare const API_URL;
+declare const API_URL: string;
 
 /**
  * Render account buttons based on current authentication state.
