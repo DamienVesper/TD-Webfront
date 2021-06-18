927831ab0aa81a9de56d8d2e108c807c08d6720c
Damien Vesper <ldamienvesper@gmail.com>
2021-06-18 17:42:32 -0500

    account btn footprint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,43 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 /**
+ * Render account buttons based on current authentication state.
+ * @returns JSX representation of account button list.
+ */
+const accountButtons = () => {
+    const loginBtns = () => {
+        return (
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a href="/auth/login" className="btn btn-block btn-success my-2 my-sm-0">Login</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/auth/login" className="btn btn-block btn-success my-2 my-sm-0">Signup</a>
+                </li>
+            </ul>
+        );
+    };
+
+    fetch(`https://throwdown.tv/authenticated`).then(data => data.json()).then(data => {
+        return data.json.isLoggedIn
+            ? (
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <a href="/auth/login" className="btn btn-block btn-success my-2 my-sm-0">Logout</a>
+                    </li>
+                </ul>
+            )
+            : loginBtns();
+    }).catch(err => {
+        // Handle the error.
+        console.log(err);
+
+        // Assume the state to be logged out.
+        return loginBtns();
+    });
+};
+
+/**
  * The header to appear at the top of every page.
  */
 class Header extends React.Component {
@@ -25,9 +62,19 @@ class Header extends React.Component {
                                     <a href="/browse" className={`nav-link ${window.location.pathname === `/browse` ? `active disabled` : ``}`}>Browse</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a href="/browse" className={`nav-link ${window.location.pathname === `/browse` ? `active disabled` : ``}`}>Browse</a>
+                                    <a href="/dashboard" className={`nav-link ${window.location.pathname === `/dashboard` ? `active disabled` : ``}`}>Dashboard</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a href="/tos" className={`nav-link ${window.location.pathname === `/tos` ? `active disabled` : ``}`}>ToS</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a href="/staff" className={`nav-link ${window.location.pathname === `/staff` ? `active disabled` : ``}`}>Our Staff</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a href="/discord" className="nav-link">Discord</a>
                                 </li>
                             </ul>
+                            {accountButtons()}
                         </div>
                     </div>
                 </nav>
