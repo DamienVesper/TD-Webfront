35cb1dc0df0238d8feecf9c57fe58cc210d35406
Damien Vesper <ldamienvesper@gmail.com>
2021-06-19 11:24:26 -0500

    fix error page

--- src/404.tsx
+++ src/404.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-// import '../public/assets/scss/pages/error.scss';
+import '../public/assets/scss/pages/error.scss';
 
 /**
  * The 404 page.
@@ -10,8 +10,8 @@ class Error404 extends React.Component {
     render = () => {
         return (
             <main className="text-center">
-                <h1 className="mt-5">404</h1>
-                <h3>Not Found</h3>
+                <h1 className="mt-5 error-header">404</h1>
+                <h3 className="error-subscript">Not Found</h3>
             </main>
         );
     }
