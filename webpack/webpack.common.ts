3ce7e36b8c99d177a7e79c9864bce75fc6d96206
Damien Vesper <ldamienvesper@gmail.com>
2021-07-21 08:45:48 -0500

    we dont use babel

--- webpack/webpack.common.ts
+++ webpack/webpack.common.ts
@@ -25,14 +25,6 @@ const config: Configuration = {
                 exclude: /node_modules/
             },
             {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: `babel-loader`,
-                options: {
-                    presets: [`@babel/preset-env`, `@babel/preset-react`]
-                }
-            },
-            {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: `asset/resource`
             },
